a578cc0f0913b73a4679b1a72c6746ac0b9f4291
Nuria.ricart <nuria.rs96@gmail.com>
2017-10-02 15:57:50 +0200

    Posar botó 1 / TextView text_phone / Fer que quan li dones 1 apareixi un 1 al TextView

--- app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
@@ -5,13 +5,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FakePhoneActivity extends AppCompatActivity {
+
+    private TextView text_phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_phone);
+
+        text_phone = (TextView) findViewById(R.id.text_phone);
+
+
+    }
+
+    public void num1 (View v){
+        Button btn_1 = (Button) v;
+        text_phone.setText("1");
     }
 
     public void clicat (View v){
